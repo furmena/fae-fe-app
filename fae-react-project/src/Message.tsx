805e03717db2f2cbda40000e95e1c78a65b722e7
Furmena <diaschithira21@gmail.com>
2024-03-10 14:09:10 +1100

    26:32 list groups!!!

---
+++ fae-react-project/src/Message.tsx
@@ -0,0 +1,7 @@
+function Message() {
+    
+    // JS XML
+    return <h1>Hello world</h1>
+}
+
+export default Message;
---
+++ fae-react-project/src/componets/ListGroup.tsx
@@ -0,0 +1,12 @@
+function ListGroup() {
+    return 
+    <ul>
+        <li className='list-group'>item 1</li>
+        <li className='list-group'>item 2</li>
+        <li className='list-group'>item 3</li>
+        <li className='list-group'>item 4</li>
+        <li className='list-group'>item 5</li>
+    </ul>
+}
+
+export default ListGroup;
